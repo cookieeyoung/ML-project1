625226eb56090cf157879400a65aa509635c7a93
mj <ireneirene1212@gmail.com>
2025-01-27 18:15:34 +0900

    test

--- Classification_mj.py
+++ Classification_mj.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import shap
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
